db1278350c537f7957c2c16f8f4aac530769c735
johardi <josef.hardi@stanford.edu>
2017-07-12 14:22:02 -0700

    Implement the use of IndexedDB via Dexie wrapper

--- search.js
+++ search.js
@@ -1,3 +1,9 @@
+var db = new Dexie("clippingDB");
+db.version(1).stores({
+  items: '[keyword+title+url],keyword'
+});
+db.open();
+
 var app = angular.module('schemaorg', [], function($provide) {
   // Fixes'history.pushState is not available in packaged apps' error message
   // Source: https://github.com/angular/angular.js/issues/11932
@@ -15,7 +21,7 @@ app.controller('SearchController', function($scope, $http) {
   $scope.doSearch = function() {
     $scope.result = []
     offset = 10;
-    repeat = 4
+    repeat = 2
     for (i = 1; i <= repeat; i++) {
       var url = 'https://www.googleapis.com/customsearch/v1' +
         '?key=AIzaSyC0tYdp3uFxDdgJO4t5hZNznH7qsBFHFRw' +
@@ -25,8 +31,25 @@ app.controller('SearchController', function($scope, $http) {
         '&num=10';
       $http.get(url).
       then(function(response) {
-          $scope.result = $scope.result.concat(response.data.items);
-          $scope.result_size = $scope.result.length;
+          var search_result = response.data.items;
+          // console.log(search_result);
+          search_result.forEach(function(element, index, array) {
+            // console.log(element);
+            db.items.add({
+              keyword: $scope.keyword,
+              title: element.title,
+              description: element.snippet,
+              url: element.link
+            }).
+            catch((err) => {
+              // NO-OP: Ignore error flags
+            });
+          });
+          db.items.where('keyword').equalsIgnoreCase($scope.keyword).toArray(result => {
+            // console.log(result);
+            $scope.result = $scope.result.concat(result);
+            $scope.result_size = result.length;
+          });
         },
         function(err) {
           // TODO: Handle error
@@ -34,9 +57,3 @@ app.controller('SearchController', function($scope, $http) {
     }
   }
 });
-
-chrome.storage.onChanged.addListener(function(changes, namespace) {
-  if (changes.data) {
-    // TODO: Do something
-  }
-})
