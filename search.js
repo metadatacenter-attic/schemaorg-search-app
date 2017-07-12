7fdd9d497b63bafa937c4778bf6a3700456121e7
johardi <josef.hardi@stanford.edu>
2017-07-12 15:43:02 -0700

    Code refactor

--- search.js
+++ search.js
@@ -1,4 +1,5 @@
 var db = new Dexie("clippingDB");
+db.delete();
 db.version(1).stores({
   items: '[keyword+title+url],keyword'
 });
@@ -17,24 +18,36 @@ var app = angular.module('schemaorg', [], function($provide) {
   });
 });
 
-app.controller('SearchController', function($scope, $http) {
+app.factory('CustomSearch', function($http) {
+  var exec = function(keyword, page) {
+    var offset = 10;
+    var url = 'https://www.googleapis.com/customsearch/v1' +
+      '?key=AIzaSyC0tYdp3uFxDdgJO4t5hZNznH7qsBFHFRw' +
+      '&cx=000084546530648272235:2a0v4vyk8nu' +
+      '&q=' + keyword +
+      '&start=' + (((page - 1) * offset) + 1) +
+      '&num=10';
+    return $http.get(url).then(
+      function(response) {
+        return response.data.items;
+      },
+      function(err) {
+        // TODO: Handle error
+      });
+  };
+  return {
+    exec: exec
+  };
+});
+
+app.controller('SearchController', function($scope, CustomSearch) {
   $scope.doSearch = function() {
     $scope.result = []
-    offset = 10;
     repeat = 2
     for (i = 1; i <= repeat; i++) {
-      var url = 'https://www.googleapis.com/customsearch/v1' +
-        '?key=AIzaSyC0tYdp3uFxDdgJO4t5hZNznH7qsBFHFRw' +
-        '&cx=000084546530648272235:2a0v4vyk8nu' +
-        '&q=' + $scope.keyword +
-        '&start=' + (((i - 1) * offset) + 1) +
-        '&num=10';
-      $http.get(url).
-      then(function(response) {
-          var search_result = response.data.items;
-          // console.log(search_result);
-          search_result.forEach(function(element, index, array) {
-            // console.log(element);
+      CustomSearch.exec($scope.keyword, i).then(
+        function(result) {
+          result.forEach(function(element, index, array) {
             db.items.add({
               keyword: $scope.keyword,
               title: element.title,
@@ -50,9 +63,6 @@ app.controller('SearchController', function($scope, $http) {
             $scope.result = $scope.result.concat(result);
             $scope.result_size = result.length;
           });
-        },
-        function(err) {
-          // TODO: Handle error
         });
     }
   }
