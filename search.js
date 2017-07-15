dec0eeea5672be91f950b15a03ffa8c3d3d505bc
johardi <josef.hardi@stanford.edu>
2017-07-15 13:23:56 -0700

    Improve and rename the filter to remove title separators

--- search.js
+++ search.js
@@ -19,13 +19,13 @@ var app = angular.module('schemaorg', ['angular.filter', 'schemaorg-constants'],
   });
 });
 
-app.filter('regex', function() {
-   return function(val){
-     console.log(val);
-     var RegExp = /^([^-|(+•]+)/;
-     var match = RegExp.exec(val);
-     return match[1];
-   };
+app.filter('removeSeparator', function() {
+  return function(input){
+    var text = input.replace(/\s-\s/g, '|');
+    var RegExp = /^([^|•:(+]+)/;
+    var match = RegExp.exec(text);
+    return match[1];
+  };
 });
 
 app.factory('CustomSearch', function($q, $http) {
